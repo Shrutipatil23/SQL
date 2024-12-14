d952885ca21b2afd39c4beca8a68d072b82a7703
Shruti Sanjay Patil <patilshrutis7597@gmail.com>
2024-12-14 19:04:48 +0530

    Create WeatherObservationStation5.sql

---
+++ HackerRank/Basic Select/WeatherObservationStation5.sql
@@ -0,0 +1,23 @@
+/*
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+
+
+SELECT City, LENGTH(City) AS City_Length 
+FROM STATION 
+ORDER BY City_Length DESC, City ASC LIMIT 1;
+
+SELECT City, LENGTH(City) AS City_Length 
+FROM STATION 
+ORDER BY City_Length ASC, City ASC LIMIT 1;
